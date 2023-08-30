fd13003ffa2fc59c0140b1492a4d659f28a843e6
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-30 21:40:29 +0530

    Finished Skills Section

--- src/components/Main/Projects.jsx
+++ src/components/Main/Projects.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Projects = () => {
-	return <div>Projects</div>;
+	return (
+		<div>
+			<div className="flex gap-6 items-center justify-start text-[32px]">
+				<h1>
+					<spam className="text-[#27ae60]">#</spam>
+					Skills
+				</h1>
+				<hr className="w-1/5 border-[1.5px] border-solid border-[#27ae60] mt-[8px]"></hr>
+				<Link to="/projects">
+					<div>View All </div>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
