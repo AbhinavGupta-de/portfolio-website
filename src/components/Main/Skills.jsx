340abfa76ec5c297b767627f19423fa5428aa590
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-30 16:40:46 +0530

    half of skills section is finished

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -8,6 +8,47 @@ const Skills = () => {
 				</h1>
 				<hr className="w-1/5 border-[1.5px] border-solid border-[#27ae60] mt-[8px]"></hr>
 			</div>
+			<div className="flex flex-col gap-5 md:w-3/5 text-[#bbbdc0] text-[16px] my-10 p-5">
+				<p>
+					{`I possess a diverse set of skills that enable me to create innovative and
+					user-friendly web applications. Here's a glimpse of my expertise:`}
+				</p>
+
+				<ul className="flex flex-col gap-5">
+					<li>
+						<strong className="text-white">Advanced Web Development:</strong>{' '}
+						Proficient in crafting dynamic and responsive web experiences using HTML,
+						CSS, and JavaScript.
+					</li>
+					<li>
+						<strong className="text-white">Frontend Frameworks:</strong> Skilled in
+						utilizing modern frontend libraries and frameworks such as React for
+						building interactive and efficient UIs.
+					</li>
+					<li>
+						<strong className="text-white">UI Frameworks:</strong> Experienced in
+						enhancing productivity and design aesthetics by incorporating frameworks
+						like Tailwind CSS, Bootstrap, Material UI, and Semantic UI.
+					</li>
+					<li>
+						<strong className="text-white">Programming Languages:</strong> Skilled in
+						programming languages including Java, Python, and C++.
+					</li>
+					<li>
+						<strong className="text-white">Version Control:</strong> Proficient in
+						using Git for efficient code management and collaboration.
+					</li>
+				</ul>
+
+				<p>
+					{`I'm continuously expanding my skill set and am currently focusing on
+					mastering backend development using Node.js, Express.js, and MongoDB to
+					create high-performance applications.`}
+				</p>
+				<p>
+					{`I also have some prior experinece in working with Spring Boot, Django, MySQL, MongoDB, and Firebase.`}
+				</p>
+			</div>
 		</div>
 	);
 };
