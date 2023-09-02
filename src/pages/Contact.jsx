0cf2127770faaa919b7019a01e603700f88fc3fc
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-09-02 12:03:40 +0530

    Also got half of the About me page done.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,20 @@
 const Contact = () => {
-	return <div>Contact</div>;
+	return (
+		<div>
+			<div className="mb-10">
+				<div className="text-[32px]">
+					<spam className="text-[#27ae60]">/</spam>about-me
+				</div>
+				<div className="mt-2">{`Let's Connect`}</div>
+			</div>
+			<div className="w-2/5">
+				{`I'm interested in internship opportunities and would love to hear about
+				 them. However, if you have other requestes or questions, don't hesitate 
+					to contact me!`}
+			</div>
+			<div></div>
+		</div>
+	);
 };
 
 export default Contact;
