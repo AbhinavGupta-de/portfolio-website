d576a808e0a95d5e8d9d5bbf46f2412d48ea77f8
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-09-02 19:21:34 +0530

    small fixes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
 const Contact = () => {
 	return (
-		<div>
+		<div className="h-screen">
 			<div className="mb-10">
 				<div className="text-[32px]">
 					<spam className="text-[#27ae60]">/</spam>about-me
 				</div>
 				<div className="mt-2">{`Let's Connect`}</div>
 			</div>
-			<div className="w-2/5">
-				{`I'm interested in internship opportunities and would love to hear about
+			<div className="flex justify-between text-[16px]">
+				<div className="md:w-2/5">
+					{`I'm interested in internship opportunities and would love to hear about
 				 them. However, if you have other requestes or questions, don't hesitate 
 					to contact me!`}
+				</div>
+				<div className="md:w-2/5 flex flex-col justify-between items-center">
+					{`You can reach me at: `}
+					<div>
+						<a href="mailto:abhinav.gupta@outlook.de">
+							<spam className="flex gap-1">
+								<MailOutlineIcon />
+								mail
+							</spam>
+						</a>
+					</div>
+					<div>
+						<FontAwesomeIcon icon="fa-brands fa-discord" />
+					</div>
+				</div>
 			</div>
-			<div></div>
 		</div>
 	);
 };
