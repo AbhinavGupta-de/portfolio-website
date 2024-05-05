350aa391c6efe80a9812e0efc117fd719c118ef2
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-05-05 15:14:46 +0530

    updated contact section home page

--- src/components/Main/AboutMe.jsx
+++ src/components/Main/AboutMe.jsx
@@ -1,4 +1,4 @@
-import Pic from '../../assets/images/abhinav.png';
+import Pic from '../../assets/images/secondPic.png';
 import { Link } from 'react-router-dom';
 
 const AboutMe = () => {
@@ -15,9 +15,8 @@ const AboutMe = () => {
 				<div className="flex gap-3 flex-col md:w-3/5 my-10 p-5 text-[#bbbdc0] text-[16px] xsm:w-full">
 					<div>{`Hello, I'm Abhinav Gupta!`}</div>
 					<div>{`I'm currently a undergraduate Computer Science student in Bangalore, India.
-				I can develop responsive websites from scratch and raise them into modren user-friendly web expereinces.`}</div>
-					<div>{`I am currently leanring MERN Stack. Diving deep in the world of web dev with the latest technologies 
-				including Typescript, Tailwind CSS, React, Next JS, Express JS, Mongo DB and much more.`}</div>
+				 I can create full stack applications with beautiful UI and database connectivity.`}</div>
+					<div>{`I am familiar with a lot of technologies but I love to work in Java as a backend developer. However I also tolerate Javascript and Python.`}</div>
 
 					<Link to="/about">
 						<div className="flex justify-start items-center gap-2 border border-solid border-[#27ae60] p-[0.30rem] w-fit mt-8">
@@ -27,7 +26,7 @@ const AboutMe = () => {
 				</div>
 				<div className="hidden w-2/5 h-2/5 md:flex justify-end">
 					{/* Removed the image for now */}
-					<img src={''} alt="" className="w-2/4" />
+					<img src={Pic} alt="" className="w-2/4" />
 				</div>
 			</div>
 		</div>
